f6da16e5b1457c058beaad77c037a2ce11e0b308
Agustin-300 <130508944+Agustin-300@users.noreply.github.com>
2023-04-13 18:51:47 -0300

    Update EJ-7-8.js

--- EJ-7-8.js
+++ EJ-7-8.js
@@ -14,5 +14,3 @@ ArrayPares = NumerosPares(array);
 console.log(ArrayPares);
 
 //E8 Crear una función que reciba un arreglo con palabras, crear un nuevo arreglo que contenga sólo las palabras que empiezan con una vocal.
-
-console.log("Hola");
