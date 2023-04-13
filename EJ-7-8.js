a5c8f1462c60fbe18f2323db6e94320b81b368f2
Agustin-300 <bargasagustin@gmail.com>
2023-04-13 18:50:18 -0300

    Update EJ-7-8.js

--- EJ-7-8.js
+++ EJ-7-8.js
@@ -15,5 +15,3 @@ console.log(ArrayPares);
 
 //E8 Crear una función que reciba un arreglo con palabras, crear un nuevo arreglo que contenga sólo las palabras que empiezan con una vocal.
 
-console.log("Prueba")
-
